a2c18ebe7b90aa652a05035882526229517a379b
wataru.matsuyama <wataru.matsuyama@accenture.com>
2024-02-25 14:01:13 +0900

    [add] vscodeとhostingの設定追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://wataruxun.github.io',
+	base: '/portfolio',
 	integrations: [mdx(), sitemap()],
 });

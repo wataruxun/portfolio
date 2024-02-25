8905d400df8e5b809b2fff00375be8127c0aec8c
wataru.matsuyama <wataru.matsuyama@accenture.com>
2024-02-25 17:48:51 +0900

    [modify] フォントパスの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wataruxun.github.io',
-  base: '/portfolio',
+  base: '/portfolio/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
